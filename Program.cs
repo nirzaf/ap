721747e335270098c95b09c288f8181f2026621a
Fazrin <nirzaf@gmail.com>
2021-08-10 15:29:27 +0530

    commit changes

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace learningProject
     {
         private static void Main(string[] args)
         {
-           Console.WriteLine("Hello World");
+            Console.Write("Input 1 : ");
+            string input = Console.ReadLine();
+            double input1;
+            if (input == "Exit" || input == "exit")
+            {
+                return;
+            }
+            else
+            {
+                input1 = Convert.ToDouble(input);
+            }
+            Console.Write("Enter the Operator +, -, * , / : ");
+            char symbol = Convert.ToChar(Console.ReadLine());
+            Console.Write("Input 2 : ");
+            double input2 = Convert.ToDouble(Console.ReadLine());
         }       
     }
 }
